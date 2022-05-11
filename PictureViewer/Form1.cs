56a6981a1aa63b0e5b866348a8728c98c61f60e0
kronosGR <geo.elgeo@gmail.com>
2022-05-11 10:09:48 +0200

    added rest functionality

--- PictureViewer/Form1.cs
+++ PictureViewer/Form1.cs
@@ -24,17 +24,20 @@ namespace PictureViewer
 
         private void closeButton_Click(object sender, EventArgs e)
         {
-
+            this.Close();   
         }
 
         private void backgroundColor_Click(object sender, EventArgs e)
         {
-
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.BackColor = colorDialog1.Color;
+            }
         }
 
         private void clearButton_Click(object sender, EventArgs e)
         {
-
+            pictureBox1.Image = null;
         }
 
         private void showButton_Click(object sender, EventArgs e)
@@ -47,7 +50,10 @@ namespace PictureViewer
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
-
+            if (checkBox1.Checked)
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            else
+                pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
         }
     }
 }
